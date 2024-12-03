afbb04b845ee14f4e4772b24580e3e291f85351b
Allan <altonallan47@gmail.com>
2024-12-03 14:18:42 +0100

    Update the Primary color

--- interio/tailwind.config.js
+++ interio/tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
+  				DEFAULT: '#7f57f1',
   				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
