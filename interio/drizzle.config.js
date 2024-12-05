4a94d9836fdc05776f7ff39a6f0a5d948b5777f8
Allan <altonallan47@gmail.com>
2024-12-05 15:47:36 +0100

    in

--- interio/drizzle.config.js
+++ interio/drizzle.config.js
@@ -2,10 +2,9 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-//   out: '',
-  schema: './config/schema',
+  schema: './config/schema.js',
   dialect: 'postgresql',
   dbCredentials: {
-    url: NEXT_PUBLIC_DATABAE_URL,
+    url: 'postgresql://interio_owner:yN8X2qvcfoCm@ep-solitary-glitter-a2bqthqt.eu-central-1.aws.neon.tech/interio?sslmode=require'
   },
 });
