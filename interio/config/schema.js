9bd8884d9d1410d91c5e7d2c3769a03713edde0b
Allan <altonallan47@gmail.com>
2024-12-05 15:47:02 +0100

    Update schema.js

--- interio/config/schema.js
+++ interio/config/schema.js
@@ -1,9 +1,9 @@
-import { pgTable } from "drizzle-orm/pg-core";
+import { pgTable, serial, varchar, integer } from "drizzle-orm/pg-core";
 
 export const Users = pgTable('users', {
-    id: serial('id').primaryKey(),
-    name: varchar('name').notNull(),
-    email: varchar('email').notNull(),
-    imageUrl: varchar('imageUrl').notNull(),
-    credits: integer('credits').default(3)
-})
+    id: serial('id').primaryKey(), // Auto-incrementing primary key
+    name: varchar('name').notNull(), // String column
+    email: varchar('email').notNull(), // String column
+    imageUrl: varchar('imageUrl').notNull(), // String column
+    credits: integer('credits').default(3) // Integer column with default value
+});
